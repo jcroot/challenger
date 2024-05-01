dd0857e034bc221433331953a1de44a0e0a15720
Jean Claude Adams <tech@arrobasoft.net>
2024-05-01 10:00:14 -0400

    add first models

--- traffic/admin.py
+++ traffic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TrafficOfficer, Person, Vehicle, TrafficViolation
+
+admin.site.register(TrafficOfficer)
+admin.site.register(Person)
+admin.site.register(Vehicle)
+admin.site.register(TrafficViolation)
--- traffic/models.py
+++ traffic/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TrafficOfficer(models.Model):
+    name = models.CharField(max_length=100)
+    badge_number = models.IntegerField(unique=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Vehicle(models.Model):
+    plate_number = models.CharField(max_length=100)
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.plate_number
+
+
+class TrafficViolation(models.Model):
+    plate_number = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    officer = models.ForeignKey(TrafficOfficer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.plate_number
