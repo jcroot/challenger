6453fdbde3f9e5710a4ece5d9b6cc59dff2f9be2
Jean Claude Adams <tech@arrobasoft.net>
2024-05-01 11:13:06 -0400

    Add initial models

--- traffic/admin.py
+++ traffic/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import TrafficOfficer, Person, Vehicle, TrafficViolation
+from .models import TrafficOfficer, Person, Vehicle, TrafficViolation, CarBrand
 
 admin.site.register(TrafficOfficer)
 admin.site.register(Person)
 admin.site.register(Vehicle)
 admin.site.register(TrafficViolation)
+admin.site.register(CarBrand)
--- traffic/models.py
+++ traffic/models.py
@@ -19,12 +19,23 @@ class Person(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return self.full_name
+
+
+class CarBrand(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Vehicle(models.Model):
     plate_number = models.CharField(max_length=100)
     owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+    brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE)
+    color = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -35,6 +46,7 @@ class Vehicle(models.Model):
 class TrafficViolation(models.Model):
     plate_number = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     officer = models.ForeignKey(TrafficOfficer, on_delete=models.CASCADE)
+    comments = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
