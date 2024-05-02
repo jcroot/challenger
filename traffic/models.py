e50f5a1dd21e0c2fdf7f9d97ffc90d8309aa7760
Jean Claude Adams <tech@arrobasoft.net>
2024-05-02 16:12:21 -0400

    Add timestamp into violations model and relation of the user with the officer

--- traffic/models.py
+++ traffic/models.py
@@ -3,13 +3,16 @@ from django.db import models
 
 
 class TrafficOfficer(models.Model):
-    name = models.CharField(max_length=100)
+    class Meta:
+        db_table = 'traffic_officer'
+
     badge_number = models.IntegerField(unique=True, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return f'{self.user.first_name} {self.user.last_name}'
 
 
 class Person(models.Model):
@@ -44,11 +47,15 @@ class Vehicle(models.Model):
 
 
 class TrafficViolation(models.Model):
+    class Meta:
+        db_table = 'traffic_violation'
+
     plate_number = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     officer = models.ForeignKey(TrafficOfficer, on_delete=models.CASCADE)
     comments = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField()
 
     def __str__(self):
         return self.plate_number
