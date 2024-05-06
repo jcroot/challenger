6cf822b3e6f8dec50b8bab7b0282408fb293cde0
Jean Claude Adams <tech@arrobasoft.net>
2024-05-06 09:39:20 -0400

    support Docker

---
+++ docker/wait-for-db.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+shift
+cmd="$@"
+
+until PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER $DB_NAME -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
